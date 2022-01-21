2557bba6f671536d5460be774c1affa42223d6fc
zevitagem <jose.victor@picpay.com>
2022-01-21 17:20:19 -0300

    Initial commit - test

---
+++ server.php
@@ -0,0 +1,37 @@
+<?php
+
+class Server
+{
+
+    public function start()
+    {
+        return [
+            'product' => 'P2P',
+            'components' => [
+                [
+                    'name' => 'Parcelamento_A',
+                    'binds' => ['Mensagem_B'],
+                    'elements' => ['input', 'select']
+                ],
+                [
+                    'name' => 'Mensagem_A', 'binds' => [], 'elements' => ['checkbox']
+                ],
+                [
+                    'name' => 'ParaQuem_A',
+                    'binds' => ['Mensagem_A', 'Parcelamento_A'],
+                    'elements' => ['textarea']
+                ]
+            ]
+        ];
+    }
+}
+
+$action = $_GET['action'];
+$result = [];
+
+$server = new Server();
+if (method_exists($server, $action)) {
+    $result = $server->{$action}();
+}
+
+echo json_encode($result);
